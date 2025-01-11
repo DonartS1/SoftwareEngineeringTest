be137e96856aa4a9317bc7c2c176c6194f149251
Donart Sylejmani <sylejmani.donart@gmail.com>
2025-01-11 15:43:01 +0100

    Added modulus operation to the calculator

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
+        System.out.println("Modulus: " + modulus(a, b)); // Test modulus
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            System.out.println("Division by zero is not allowed");
+            return 0;
+        }
+    }
+
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+}
